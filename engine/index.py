157a90b4fcc4be52282fb3f7e1397263c67a8434
juldhy <julien.dhyne+1@gmail.com>
2026-07-02 09:04:22 +0200

    feat: add search index

---
+++ engine/index.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+import json
+from .tokenize import tokenize
+
+
+class SearchIndex:
+    """
+    A class that builds index on initialization and allows searching for items based on tokens.
+    """
+
+    def __init__(self, json_file: str):
+        self.json_file = json_file
+        self.index = self._build_index()
+
+    def _build_index(self) -> dict[str, set[str]]:
+        with open(self.json_file, "r", encoding="utf-8") as f:
+            data = json.load(f)
+
+        idx = defaultdict(set)
+
+        for item in data:
+            for token in tokenize(item["name"]):
+                idx[token].add(item["id"])
+
+            for tag in item["tags"]:
+                idx[tag].add(item["id"])
+
+        return dict(idx)
+
+    def search(self, token: str) -> set[str]:
+        return self.index.get(token.lower().strip(), set())
