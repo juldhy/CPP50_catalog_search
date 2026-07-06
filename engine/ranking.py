310725a3fba8a856b7e0e1fd87ed393e233956ca
Alain <neroliabali@gmail.com>
2026-07-06 14:39:51 +0200

    [IMP] ranking: add filter_fn optional arg to search()

--- engine/ranking.py
+++ engine/ranking.py
@@ -24,7 +24,7 @@ def compute_score(product, query_tokens : set[str]) -> float:
             + (1 / math.log2(product['sales_rank'] + 2)) * 0.3
 
 
-def search(query: str, top_k: int = 10) -> list[str]:
+def search(query: str, top_k: int = 10, filter_fn = None) -> list[str]:
     """
     This function
     - tokenises the query the same way as the index
@@ -34,7 +34,11 @@ def search(query: str, top_k: int = 10) -> list[str]:
             + (stock > 0) * 0.2
             + (1 / log2(sales_rank + 2)) * 0.3
     - Returns the top_k highest-scoring products
-    :pre catalog invert index of the form { token:str : set{productid:str} }
+
+    :pre catalog invert index of the form { token:str : set{productid:str} } is accessible
+    :pre catalog is accessible
+
+    :param filter_fn: a filter_fn to apply to the candidate product ID, before scoring and sorting.
     :param query: The query
     :param top_k: The max size of the return list
     :return: the top_k highest-scoring products as an ordered list
@@ -42,11 +46,10 @@ def search(query: str, top_k: int = 10) -> list[str]:
 
     catalog = search_index.catalog
     query_tokens = set(tokenize(query))
+    # Get the candidate product ID's as the union of the index entries of each query tokens
     candidates = reduce(lambda result, token : result.union(search_index.index.get(token, set())), query_tokens, set())
+    # Find the top_k (candidate product ID, score) according to the computed score, but return the product IDs only
     return list(map(lambda id_score:catalog.get(id_score[0]),
                     heapq.nlargest(top_k,
-                          map(lambda candidate_id: (candidate_id, compute_score(catalog.get(candidate_id), query_tokens)), candidates),
+                          map(lambda candidate_id: (candidate_id, compute_score(catalog.get(candidate_id), query_tokens)), filter(filter_fn, candidates)),
                           key=lambda x: x[1])))
-
-
-
